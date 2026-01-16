a9570a1879445ee32cf9727555d2059d0e9e057a
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2026-01-16 10:35:09 +0100

    Updating the configuration by using helm charts for gitlab

--- push-yaml.sh
+++ push-yaml.sh
@@ -1,3 +1,4 @@
 scp -r -i ../SSH/sisyphe_key gitlab/* sisyphe@10.0.1.4:/home/gitlab
 scp -r -i ../SSH/sisyphe_key registry/* sisyphe@10.0.1.4:/home/registry
 scp -r -i ../SSH/sisyphe_key app/* sisyphe@10.0.1.4:/home/app
+scp -r -i ../SSH/sisyphe_key runner/* sisyphe@10.0.1.4:/home/runner
